afdd6df50d75eb7d0841febc6afa0a71524edee8
fivefootwan <wanvipa.wb@gmail.com>
2025-06-30 13:27:00 +0700

    added frames and layout

--- reddit-app/src/App.js
+++ reddit-app/src/App.js
@@ -5,19 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Reddit App</h1>
+        <form className='Search-Bar'>
+          <input placeholder="Input a term" className='Search-Input'/>
+          <select className='Search-Dropdown'> 
+            <option value="default">Default</option>
+            <option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+          </select>
+          <button type="submit" className="Search-Button">Search</button>
+        </form>
       </header>
+
+      <div className='Content-Body'>
+        <h2>Results</h2>
+        <div className='Result-Card'>
+          <p>this is</p>
+
+        </div>
+
+      </div>
+
+
     </div>
   );
 }
